0376c5992956346fc342891faae96327a4a96f63
RemiDrt <rd.duret@hotmail.fr>
2020-05-27 16:46:47 +0200

    vérification du type de fichier choisi

--- NRI_extractor.py
+++ NRI_extractor.py
@@ -8,12 +8,15 @@ import os
 repCourant = os.getcwd()
 root = Tk()
 root.filename = filedialog.askopenfilename(initialdir=repCourant, title = "Selectionner le fichier nri", filetypes = (("nri files","*.nri"), ("all files","*.*")))
-print(root.filename)
-#opening the file in read only
-NRI_File = open(root.filename, encoding="utf-8")
-#creer une liste avec toutes les lignes
-texte = NRI_File.readlines()
-#fermer le fichier pour éviter les problèmes
-NRI_File.close()
-nri = NRI(texte)
-AfficherNRI(nri)
+extension = root.filename.split(".")[1]
+if extension != "nri" :
+    print("erreur, mauvais type de fichier")
+else :
+    #opening the file in read only
+    NRI_File = open(root.filename, encoding="utf-8")
+    #creer une liste avec toutes les lignes
+    texte = NRI_File.readlines()
+    #fermer le fichier pour éviter les problèmes
+    NRI_File.close()
+    nri = NRI(texte)
+    AfficherNRI(nri)
