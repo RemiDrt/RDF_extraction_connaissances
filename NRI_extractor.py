8b3c35d748a0db7b94642ceae9341091655690d1
RemiDrt <rd.duret@hotmail.fr>
2020-05-27 16:42:29 +0200

    ajout fenêtre graphique de choix de fichier

--- NRI_extractor.py
+++ NRI_extractor.py
@@ -2,8 +2,15 @@
 #coding=utf-8
 #import des fonctions d'extractions spécifiques aux fichier NRI
 from Extractors import *
+from tkinter import filedialog
+from tkinter import *
+import os
+repCourant = os.getcwd()
+root = Tk()
+root.filename = filedialog.askopenfilename(initialdir=repCourant, title = "Selectionner le fichier nri", filetypes = (("nri files","*.nri"), ("all files","*.*")))
+print(root.filename)
 #opening the file in read only
-NRI_File = open("testfile.nri", encoding="utf-8")
+NRI_File = open(root.filename, encoding="utf-8")
 #creer une liste avec toutes les lignes
 texte = NRI_File.readlines()
 #fermer le fichier pour éviter les problèmes
