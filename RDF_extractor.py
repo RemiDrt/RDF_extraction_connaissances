845a5d55f64dfd326e2e4a8011cc0910f8b37b0c
RemiDrt <rd.duret@hotmail.fr>
2020-05-28 15:57:55 +0200

    extraction des données d'un graphe rdf stockés dans un objet graphe

--- RDF_extractor.py
+++ RDF_extractor.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python
 #coding=utf-8
+import rdflib
+graphe = rdflib.Graph() #l'objet graphe pour l'instant vide qui contientdra tous les triplets
+print(len(g))
+graphe.parse("field.ttl", format="turtle") #on lui ajouter tous les triplets venant du fichier field.ttl
+print("done")
+print(len(g))
+
+for sujet, predica, objet in graphe :
+    print("----------------\n")
+    print(sujet)
+    print(predica)
+    print(objet)
+    print("----------------\n")
